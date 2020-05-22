3965768df0647ee2adf58f2a96739d914e77cbb9
SoraKukyo <borormeyphalla@gmail.com>
2020-05-22 05:10:03 -0400

    second commit

--- newsite/polls/models.py
+++ newsite/polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 # Create your models here.
@@ -13,7 +13,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return now - timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
